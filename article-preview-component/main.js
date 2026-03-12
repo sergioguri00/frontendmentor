eb5d39adc13847f4216542438b43bbb9323cd0ac
SergioG <sergio.guerrero@anconnect.com>
2026-03-12 15:50:54 +0100

    refactor share functionality and improve layout for article preview component

--- article-preview-component/main.js
+++ article-preview-component/main.js
@@ -5,27 +5,48 @@ const span = document.getElementById("span");
 const shareIcons = document.getElementById("share-icons");
 const shareButton = document.getElementById("share-button");
 const buttonImg = document.getElementById("button-img");
+const shareBox = document.getElementById("share-box");
+const dialog = document.getElementById("dialog");
 
 shareButton.addEventListener("click", () => {
-  if (aside.classList.contains("inactive-aside")) {
-    aside.classList.remove("inactive-aside");
-    aside.classList.add("active-aside");
+  console.log(window.innerWidth);
+  if (window.innerWidth >= 1440) {
+    if (shareBox.classList.contains("inactive")) {
+      shareBox.classList.add("active");
+      shareBox.classList.remove("inactive");
 
-    span.style.display = "block";
-    shareIcons.style.display = "flex";
-    buttonImg.src = window.location.href + "images/icon-share-active.svg";
+      buttonImg.src = window.location.href + "images/icon-share-active.svg";
 
-    articleInfo.style.display = "none";
-    avatar.style.display = "none";
+      dialog.style.display = "flex";
+    } else {
+      shareBox.classList.add("inactive");
+      shareBox.classList.remove("active");
+
+      buttonImg.src = window.location.href + "images/icon-share.svg";
+
+      dialog.style.display = "none";
+    }
   } else {
-    aside.classList.remove("active-aside");
-    aside.classList.add("inactive-aside");
+    if (aside.classList.contains("inactive-aside")) {
+      aside.classList.remove("inactive-aside");
+      aside.classList.add("active-aside");
 
-    articleInfo.style.display = "flex";
-    avatar.style.display = "block";
-    buttonImg.src = window.location.href + "images/icon-share.svg";
+      span.style.display = "block";
+      shareIcons.style.display = "flex";
+      buttonImg.src = window.location.href + "images/icon-share-active.svg";
 
-    span.style.display = "none";
-    shareIcons.style.display = "none";
+      articleInfo.style.display = "none";
+      avatar.style.display = "none";
+    } else {
+      aside.classList.remove("active-aside");
+      aside.classList.add("inactive-aside");
+
+      articleInfo.style.display = "flex";
+      avatar.style.display = "block";
+      buttonImg.src = window.location.href + "images/icon-share.svg";
+
+      span.style.display = "none";
+      shareIcons.style.display = "none";
+    }
   }
 });
